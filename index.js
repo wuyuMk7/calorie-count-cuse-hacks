714f81adb60c5f04c9a172fad38a901242dea030
hyang <hymeldon@gmail.com>
2018-04-21 19:20:26 -0400

    Add router files.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const express = require('express'),
       app = express();
 
+app.use(require('./login'));
+app.use(require('./file'));
+
 app.use(express.static('public'));
 app.get('/', function(req, res, next) {
     
